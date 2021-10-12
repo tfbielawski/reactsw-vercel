ea93710450b815969fed79372f9ec4492ac4f436
tfbielawski <tfbielawski@protonmail.com>
2021-10-12 01:36:12 -0400

    adusting font

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ export default function Home() {
     return (
         <div>
             <p>WELCOME TO THE STAR WARS API</p>
+            <p> Click on a link above to get started</p>
         </div>
     )
 }
